9acd6b8924435f7589f26d8cfbf03fb03c21de5f
sunghun <nokdonwithkj211@gmail.com>
2023-11-30 18:28:36 +0900

    feat(TimeSubwayPath): 가중치가 시간이 노선도를 생성하는 클래스

--- src/main/java/subway/domain/subwayPath/TimeSubwayPath.java
+++ src/main/java/subway/domain/subwayPath/TimeSubwayPath.java
@@ -1,5 +1,39 @@
 package subway.domain.subwayPath;
 
-public class TimeSubwayPath {
+import java.util.List;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+import subway.domain.Line;
+import subway.domain.LineRepository;
+import subway.domain.Station;
+import subway.domain.StationRepository;
 
+public class TimeSubwayPath {
+    private final WeightedMultigraph<Station, DefaultWeightedEdge> distanceTimeGraph;
+
+    public TimeSubwayPath() {
+        this.distanceTimeGraph = new WeightedMultigraph(DefaultWeightedEdge.class);
+        setUpDistance();
+    }
+
+    private void setUpDistance() {
+        List<Station> stations = StationRepository.stations();
+        for (Station station : stations) {
+            distanceTimeGraph.addVertex(station);
+        }
+
+        List<Line> lines = LineRepository.lines();
+        for (Line line : lines) {
+            List<TimeEdge> timeEdges = line.getTimeEdges();
+            for (TimeEdge timeEdge : timeEdges) {
+                DefaultWeightedEdge edge = distanceTimeGraph.addEdge(timeEdge.getSource(),
+                        timeEdge.getTarget());
+                distanceTimeGraph.setEdgeWeight(edge, timeEdge.getWeight());
+            }
+        }
+    }
+
+    public WeightedMultigraph<Station, DefaultWeightedEdge> getDistanceTimeGraph() {
+        return distanceTimeGraph;
+    }
 }
